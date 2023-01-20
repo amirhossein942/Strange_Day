fb949be48e8cb6937f29134b917a26295f04eacd
amirhossein942 <83388763+amirhossein942@users.noreply.github.com>
2023-01-20 02:36:02 -0500

    ass

--- Strange Day/Assets/HealthBar.cs
+++ Strange Day/Assets/HealthBar.cs
@@ -8,16 +8,26 @@ public class HealthBar : MonoBehaviour
     public const float MaxHealth = 9f;
     public Image healthBar;
     public PlayerController playerController;
+    RectTransform canvas; 
     // Start is called before the first frame update
     void Start()
     {
         healthBar = GetComponent<Image>();
         playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+        canvas = GetComponentInParent<Canvas>().GetComponent<RectTransform>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         healthBar.fillAmount = playerController.Health / MaxHealth;
+
+    }
+
+    void LateUpdate()
+    {
+
+
     }
 }
