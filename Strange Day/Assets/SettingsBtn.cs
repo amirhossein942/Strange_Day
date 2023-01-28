a7ea8aa4ed875f437078f800e84c28bf1d19d930
amirhossein942 <83388763+amirhossein942@users.noreply.github.com>
2023-01-27 23:04:31 -0500

    menu

---
+++ Strange Day/Assets/SettingsBtn.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsBtn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void settingsBtn() { 
+    
+        Time.timeScale = 0f;
+    }
+}
---
+++ Strange Day/Assets/SettingsMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void settingsmenu() { 
+    
+        Time.timeScale= 1.0f;   
+    }
+}
